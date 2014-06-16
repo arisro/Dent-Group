0b3866c7da9d1abce91be19f484fc7b3882c6331
Aris BUZACHIS <buzachis.aris@gmail.com>
2014-06-16 21:26:18 +0000

    fixed migration

--- db/migrate/20140616100108_create_news_categories.rb
+++ db/migrate/20140616100108_create_news_categories.rb
@@ -2,7 +2,7 @@ class CreateNewsCategories < ActiveRecord::Migration
   def change
     create_table :news_categories do |t|
       t.string :ident
-      t.boolean :deleted, default: :false
+      t.boolean :deleted, default: false
 
       t.timestamps
     end
