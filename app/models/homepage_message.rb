3257dd352ef6f16a90e37ec116eb4381cbdcdd53
Aris BUZACHIS <buzachis.aris@gmail.com>
2014-06-25 21:02:04 +0000

    ...

--- app/models/homepage_message.rb
+++ app/models/homepage_message.rb
@@ -1,4 +1,4 @@
 class HomepageMessage < ActiveRecord::Base
   belongs_to :homepage_messages_category
-	default_scope order('priority DESC, published_at DESC')
+	default_scope order('priority DESC, published_at DESC, id DESC')
 end
